577e7894e2c6c3ba28700041ea6cf15561cb2530
Cyryl <folkbread@gmail.com>
2022-04-11 23:16:16 +0200

    added new code 11.04

--- projects/project_1/js/project_1.js
+++ projects/project_1/js/project_1.js
@@ -1,22 +1,22 @@
 let numberOfFilms = []; // ilość obejrzanych filmów
 let lastFilm = [], rateThisFilm = []; //ostatni film ( nazwa ), ocena filmu
 
-numberOfFilms[0] = +prompt('Сколько фільмов вы уже посмотрелі?'); // pytanie do użytkownika o ilości obejrzanych filmów
-lastFilm[0] = prompt('Какой был последній просмотренный фільм?'); // pytanie do użytkownika o nazwie ostatniego filmu
-rateThisFilm[0] = +prompt('Какую оценку Вы бы поставілі этому фільму?');  // pytanie dotyczące oceny danego filmu
+numberOfFilms[0] = 3; // pytanie do użytkownika o ilości obejrzanych filmów
+lastFilm[0] = "Matrix"; // pytanie do użytkownika o nazwie ostatniego filmu
+rateThisFilm[0] = 7;  // pytanie dotyczące oceny danego filmu
 
 const personalMovieDB = { // obiekt dla naszej aplikacji
     count: Number(numberOfFilms),
-    movies:{[String(lastFilm)]: String(rateThisFilm)},
+    movies:{
+        [String(lastFilm)]: String(rateThisFilm)
+    },
     actors: {},
     genres: [],
     privat: false,
 };
 
-alert(`Значіт, ты посмотрел ${personalMovieDB.count} фільма(ов)`);
-alert(`Ostatni film i jego ranking - ${personalMovieDB.movies.lastFilm}`);
+console.log(personalMovieDB.movies);
 
 //+prompt('Сколько фільмов вы уже посмотрелі?')
 //prompt('Какой был последній просмотренный фільм?')
 //+prompt('Какую оценку Вы бы поставілі этому фільму?');
-
