c99943469e3d9aed2e1b39d84350e3c447d3bd46
Cyryl <folkbread@gmail.com>
2022-04-23 22:19:56 +0200

    exercises

--- projects/Mozilla_works/sandpit_mozilla/playWithLoops.js
+++ projects/Mozilla_works/sandpit_mozilla/playWithLoops.js
@@ -5,10 +5,105 @@
 //     numb++;
 // }
 
+// let changeNumber = 100;
 
-for (let i = 0; i <= 7; i++) {
-   console.log('=', i);
-    for (let j = 0; j <= 3; j++) {
-        console.log('==', j);
+//  for (let count = 0; count <= changeNumber; count++) {
+//      console.log(count);
+//  }
+
+let number = 5;
+for (let count = 1; count < 8; count++) {
+    console.log(count);
+    if ( count === 5) {
+        continue;
     }
+
+}
+
+// let result = ''; // rezultat pracy naszych loopów
+// let amountEnter = 7; // ilość naszych przejść na nową linię
+
+// for (let firstOne = 0; firstOne < amountEnter; firstOne++) { // pierwszy loop odpowiada za tworzenie nowych linii
+  
+//     for (let secondOne = 0; secondOne < firstOne; secondOne++) { // drugi loop odpowiada za dodawanie ilości gwizdek
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// first: for (let i = 2; i < 5; i++) {
+//     console.log(`First tag: ${i}`);
+//     for ( let j = 1; j < 4; j++) {
+//         console.log(`Second tag: ${j}`);    
+//         for ( let k = 0; k < 3; k++) {
+//             if (k === 2) break first;
+//             console.log(`Third tag: ${k}`);
+            
+//         }
+//     }
+// }
+
+// Место для первой задачи
+function firstTask() {
+    // Пишем решение вот тут
+    for (let numberForLoop = 5; numberForLoop <= 10; numberForLoop++) {
+        console.log(numberForLoop);
+    }
+    return numberForLoop;
+}
+let x = firstTask();
+console.log(x);
+
+
+let numberForLoop = 5;
+do {
+    console.log(numberForLoop);
+    numberForLoop++;
+} while (numberForLoop <= 10);
+
+
+// Место для второй задачи
+function secondTask() {
+    // Пишем решение вот тут
+    
+    
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    // Пишем решение вот тут
+    
+    
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    // Пишем решение вот тут
+    
+    
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    // Пишем решение вот тут
+    
+    
+    // Не трогаем
+    return arrayOfNumbers;
 }
