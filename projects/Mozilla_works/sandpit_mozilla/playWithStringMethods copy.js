83074d4239c959879e460bd626fbd1eea8115074
Cyryl <folkbread@gmail.com>
2022-05-04 21:07:48 +0200

    work with function

--- projects/Mozilla_works/sandpit_mozilla/playWithStringMethods copy.js
+++ projects/Mozilla_works/sandpit_mozilla/playWithStringMethods copy.js
@@ -35,3 +35,77 @@ console.log(firstString.toUpperCase()); // zwraca ciąg w górnym registrze
 // ==================================
 
 console.log(firstString.replace('John', 'Cyryl'));
+
+// ==================================
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(`2 + 3 = ${sum(2, 3)}.`); // 
+
+// ==================================
+
+// loop while szukający wszystkie pozycje występowania podanego znaczenia
+
+let str = 'Ослик Иа-Иа посмотрел на виадук'; // baza, gdzie szukamy
+let target = 'Иа'; // znaczenie, które szukamy
+let pos = 0; // pozycja szukanego targetu
+let count = 0;
+
+while (true) {
+    let foundPos = str.indexOf('Иа', 1);
+    if ( foundPos === -1) break;
+    count++;
+
+    console.log(`Znaleziono tutaj: ${foundPos}. W sumie występuje tyle razy: ${count}`);
+    // for (let i = 0; i < 1; i++) {
+    //     const newObj = {};
+    //     newObj.foundPos = foundPos;
+    //     console.log(newObj.foundPos);
+    // }
+    pos = foundPos + 1;
+}
+
+// let str = "Ослик Иа-Иа посмотрел на виадук";
+// let target = "Иа";
+
+// let pos = -1;
+// while ((pos = str.indexOf(target, pos + 1)) != -1) {
+//   alert( pos );
+// }
+
+// ==================================
+
+let newWord = "Hi, my name's Kiryl";
+
+console.log(newWord.slice(1, 6)); // metoda slice(star[, end]) - wycina od start do end ( może być bez end);
+console.log(newWord.slice(-5)); // 
+console.log(newWord.substr(7, 6));
+
+
+
+// function checkSpam(str) {
+//     if ( str === 'viagra') {
+//         return true;
+//     } else if (str == 'xx')
+// }
+
+let newStr = '';
+
+for ( let i = 65; i <= 220; i++) {
+    newStr += String.fromCodePoint(i);
+}
+
+console.log(newStr);
+
+
+// =================
+const c1 = -1.4; // Math.ceil = zaokrągla w górę,
+const c2 = -1.6; // Math.floor - zaokrągla w dół,
+const c3 = -6.7; // Math.round - zaokrągla do najbliższej całej liczby,
+const c4 = -1111.44444; // Math.trunc - usuwa liczbę po kropce
+const c5 = 3;
+
+console.log(`1. ${Math.ceil(c1)},\n2. ${Math.floor(c2)},\n3. ${Math.round(c3)},\n4. ${Math.trunc(c4)},\n5. ${c5};`);
+
--- projects/project_1/js/project_1_ClearVersion.js
+++ projects/project_1/js/project_1_ClearVersion.js
@@ -1,52 +1,108 @@
-const numberOfFilms = +prompt('Сколько фільмов вы уже посмотрелі?', ''); // ilość obejrzanych filmów
+let numberOfFilms; // ilość obejrzanych filmów
 // + przed prompt oznacza, że pytanie będzie prosiło wyłącznie liczby
-const personalMovieDB = { // obiekt dla naszej aplikacji
+
+function start() {
+    
+    numberOfFilms = +prompt('Сколько фільмов вы уже посмотрелі?', '');
+
+    while (numberOfFilms == '' || numberOfFilms === null || isNaN(numberOfFilms))  {
+        numberOfFilms = +prompt('Сколько фільмов вы уже посмотрелі?', '');
+    }
+}
+
+start();
+
+let personalMovieDB = { // obiekt dla naszej aplikacji
     count: [numberOfFilms],
     movies: {},
     actors: {},
     genres: [],
-    privat: false,
+    privat: true,
 };
-let i = 0;
 
-first: do { // dodana została etykieta (label), do której można się zwrócić
-    const lastFilm = prompt('Какой был последній просмотренный фільм?', '');
-    if (lastFilm == false || lastFilm === null) { 
-/*
-Górny if zawiera warunki, gdy zmienna z odpowiedzią będzie miała pustą (false) lub anulowaną odpowiedź (null)
-*/
-        alert('Нельзя строк пустых');
-        continue first; 
-/*
-W przypadku spełnionego warunku wyskakuje alarm i loop wraca do początku (dzięki połączonej etykiecie)
-*/
-    } else if (lastFilm.length >= 50) {
-        alert('Слішком большое названіе!');
-        continue first;
-    }
-    for ( let x = 0; x < 1; x++) {
-        const rateThisFilm = +prompt('Какую оценку Вы бы поставілі этому фільму?', '');
-        if (rateThisFilm > 10 || rateThisFilm === null || rateThisFilm == false) {
-            alert('Оценка должна быть от 0 до 10.');
-            x--;
-/*
-inkrement (x++) dodaje liczbę do naszej wartości (x), a przy spełnieniu warunka, to dekrement (x--) cofa
-*/
+function showMyDB() { // sprawdza czy user wybrał true or false w sprawie publiczności ankiety
+
+    startgame: do {
+        let questionnairePrivate = prompt('Скажи, пожалуйста, твоя анкета может быть публичной?', 'Правда ИЛИ Ложь');
+        if (questionnairePrivate.toLowerCase() === "правда") {
+            personalMovieDB.privat = false;
+            alert('Прекрасно, идём дальше!');
+        } else if(questionnairePrivate.toLowerCase() === "ложь") {
+            personalMovieDB.privat = true;
+            alert('Окей, твоя анкета не будет видна');
+            break;
         } else {
-            personalMovieDB.movies[lastFilm] = rateThisFilm;
+            alert('Пожалуйста, дай точный ответ =)');
+            continue startgame;
         }
-    }
-    // personalMovieDB.movies[lastFilm] = rateThisFilm;
-    i++;    
-} while ( i < 2 );
+    
+    } while (personalMovieDB.privat === true);
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    alert('Вы кіноман!');
-} else {
-    alert('Проізошла ошібка!');
 }
+
+function dontShowDB() { // w przypadku false w poprzedniej funkcji program na końcu nie wyświetli tabeli(obiektu)
+    if (personalMovieDB.privat === true) {
+        alert('Ошибка, анкета закрыта для просмотра!');
+        personalMovieDB = null;
+    }
+ }
+
+
+function questionForUsers() {
+
+    let i = 0;
+
+    first: do { // dodana została etykieta (label), do której można się zwrócić
+        const lastFilm = prompt('Какой был последній просмотренный фільм?', '');
+        if (lastFilm == false || lastFilm === null) { 
+    /*
+    Górny if zawiera warunki, gdy zmienna z odpowiedzią będzie miała pustą (false) lub anulowaną odpowiedź (null)
+    */
+            alert('Нельзя строк пустых');
+            continue first; 
+    /*
+    W przypadku spełnionego warunku wyskakuje alarm i loop wraca do początku (dzięki połączonej etykiecie)
+    */
+        } else if (lastFilm.length >= 50) {
+            alert('Слішком большое названіе!');
+            continue first;
+        }
+        for ( let x = 0; x < 1; x++) {
+            const rateThisFilm = +prompt('Какую оценку Вы бы поставілі этому фільму?', '');
+            if (rateThisFilm > 10 || rateThisFilm === null || rateThisFilm == false) {
+                alert('Оценка должна быть от 0 до 10.');
+                x--;
+    /*
+    inkrement (x++) dodaje liczbę do naszej wartości (x), a przy spełnieniu warunka, to dekrement (x--) cofa
+    */
+            } else {
+                personalMovieDB.movies[lastFilm] = rateThisFilm;
+            }
+        }
+        // personalMovieDB.movies[lastFilm] = rateThisFilm;
+        i++;    
+    } while ( i < 2 );
+
+}
+
+function yourInvolvement() {
+
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        alert('Вы кіноман!');
+    } else {
+        alert('Проізошла ошібка!');
+    }
+
+}
+
+showMyDB();
+questionForUsers();
+yourInvolvement();
+
+dontShowDB();
+
 console.log(personalMovieDB);
