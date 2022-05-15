ae6bf99efd5b49a4ca6d3cb494ce0a228ca95eba
Cyryl <folkbread@gmail.com>
2022-05-15 22:21:21 +0200

    little bit more

--- projects/Mozilla_works/sandpit_mozilla/homework_Obj.js
+++ projects/Mozilla_works/sandpit_mozilla/homework_Obj.js
@@ -4,29 +4,59 @@ const personalPlanPeter = {
     skills: {
         languages: ['ru', 'eng'],
         programmingLangs: {
-            js: '20%',
-            php: '10%'
+            // js: '20%',
+            // // php: '10%'
         },
         exp: '1 month'
     }
 };
 
-function showExperience(plan) {
+const {skills: {
+    programmingLangs,
+    }
+} = personalPlanPeter;
+
+console.log(programmingLangs == {});
+
+// zadanie #1
+
+// function showExperience(plan) {
     
-    const {
-        name,
-        age,
-        skills: {
-            languages,
-            programmingLangs,
-            exp
+//     const {
+//         name,
+//         age,
+//         skills: {
+//             languages,
+//             programmingLangs,
+//             exp
+//         }
+//     } = plan;
+
+//     return exp;
+
+// }
+
+// console.log(showExperience(personalPlanPeter));  
+
+
+// zadanie #2
+
+
+function showProgrammingLangs(anotherPlan) {
+    
+    const {skills: {
+        programmingLangs,
+    }
+    } = anotherPlan;
+    
+    
+    if (!Object.keys(programmingLangs).length === 0) {
+        for ( let key in programmingLangs ) {
+            console.log(`Язык ${key} изучен на ${programmingLangs[key]}\n`);
         }
-    } = plan;
-
-
-    return exp;
-
+    } else {
+        console.log('Не знаешь ещё языков, либо не вписал инфо');
+    }
 }
 
-
-console.log(showExperience(personalPlanPeter));  
+showProgrammingLangs(personalPlanPeter);
