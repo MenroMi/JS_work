79b9829ed6fd8529f02a9dca91fb91518b4ecdec
Cyryl <folkbread@gmail.com>
2022-05-16 22:25:24 +0200

    New work with Objects

--- projects/Mozilla_works/sandpit_mozilla/homework_Obj.js
+++ projects/Mozilla_works/sandpit_mozilla/homework_Obj.js
@@ -1,38 +1,53 @@
 const personalPlanPeter = {
     name: "Peter",
-    age: "29",
+    age: "30",
     skills: {
-        languages: ['ru', 'eng'],
+        languages: ['ru', 'ua'],
         programmingLangs: {
-            // js: '20%',
-            // // php: '10%'
+            js: '20%',
+            php: '10%',
+            ruby: '50%',
         },
         exp: '1 month'
-    }
+    },
+    // zadanie #3
+    // showAgeAndLangs: function(plan) {
+    //     const { skills: {languages: [item1, item2]}}= plan;
+    //     let str = `Мне ${plan.age} и я владею языками: ${item1.toUpperCase()} ${item2.toUpperCase()}`;
+    //     str += ' ';
+    //     return str;
+    // }
 };
 
-const {skills: {
-    programmingLangs,
-    }
-} = personalPlanPeter;
+// personalPlanPeter.age = '25';
+// personalPlanPeter.skills.languages = ['ru', 'eng'];
 
-console.log(programmingLangs == {});
+// console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+// const {skills: {
+//     programmingLangs,
+//     }
+// } = personalPlanPeter;
+
+// programmingLangs.python = '30%';
+
+// console.log(personalPlanPeter.skills.programmingLangs);
 
 // zadanie #1
 
 // function showExperience(plan) {
     
-//     const {
-//         name,
-//         age,
-//         skills: {
-//             languages,
-//             programmingLangs,
-//             exp
-//         }
-//     } = plan;
+    // const {
+    //     name,
+    //     age,
+    //     skills: {
+    //         languages,
+    //         programmingLangs,
+    //         exp
+    //     }
+    // } = plan;
 
-//     return exp;
+    // return exp;
 
 // }
 
@@ -49,14 +64,62 @@ function showProgrammingLangs(anotherPlan) {
     }
     } = anotherPlan;
     
-    
-    if (!Object.keys(programmingLangs).length === 0) {
-        for ( let key in programmingLangs ) {
-            console.log(`Язык ${key} изучен на ${programmingLangs[key]}\n`);
-        }
-    } else {
+    let newString = '';
+
+    if (Object.keys(programmingLangs).length === 0) {
         console.log('Не знаешь ещё языков, либо не вписал инфо');
+    } else {
+        for ( let key in programmingLangs ) {
+            newString += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+        }
+    }
+    return newString;
+}
+
+console.log(showProgrammingLangs(personalPlanPeter));
+
+// ================================
+// zadanie #3
+
+let schedule = {
+    name: 'John'
+};
+
+const isEmpty = obj => {
+    if ( Object.keys(obj).length === 0) { // zmieniamy na array i sprawdzamy długość
+    
+        // można również sprawdzić bezpośrednio obiekt za pomocą loopa:
+        // for ( let key in obj) {
+        //    return false;
+        // }
+
+        return true;
+    } else {
+        return false;
+    }
+};
+
+console.log(isEmpty(schedule));
+
+// =====================
+// zadanie #4
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+    for ( let key in obj ) {
+        if ( typeof(obj[key]) === 'number' ) {
+            obj[key] = obj[key] * 2;
+        } else {
+            continue;
+        }
     }
 }
 
-showProgrammingLangs(personalPlanPeter);
+multiplyNumeric(menu);
+
+console.log(menu);
