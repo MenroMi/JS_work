37e772fbeffe328acade3c49d6ceccb0fa022ee2
Cyryl <folkbread@gmail.com>
2022-05-22 22:28:18 +0200

    prototype

--- projects/Mozilla_works/sandpit_mozilla/playWithPrototypes.js
+++ projects/Mozilla_works/sandpit_mozilla/playWithPrototypes.js
@@ -3,6 +3,9 @@ let vegeAnimal = Object.create(null);
 vegeAnimal.eats = true;
 vegeAnimal.run = true;
 vegeAnimal.predator = false;
+vegeAnimal.sleep = function() {
+    return 'Sleep a few hours';
+};
 
 
 const dog = Object.create(vegeAnimal); // stworzyliśmy obiekt psa dodawając go do naszego wcześniej stworzonego obiektu
@@ -11,4 +14,28 @@ dog.food = 'season vegetables';
 
 dog.__proto__ = vegeAnimal; // this function of JS is deprecated
 
-console.log(dog.predator);
+console.log(dog.sleep());
+
+// =====================
+
+let user = {
+    name: 'John',
+    age: 30
+};
+
+user.sayHi = function() {
+    return 'Hello!';
+};
+
+console.log(user.sayHi());
+
+// =====================
+
+let userName = {
+    name: "Джон",
+    hi() { console.log(this.name); }
+  };
+  
+  // разделим получение метода объекта и его вызов в разных строках
+  let hi = userName.hi;
+  hi(); 
