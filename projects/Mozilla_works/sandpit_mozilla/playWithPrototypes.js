8d30dd2e764d82cebecf25407ce9e60d00fb0a0e
Cyryl <folkbread@gmail.com>
2022-05-23 20:50:04 +0200

    prototypes

--- projects/Mozilla_works/sandpit_mozilla/playWithPrototypes.js
+++ projects/Mozilla_works/sandpit_mozilla/playWithPrototypes.js
@@ -39,3 +39,63 @@ let userName = {
   // разделим получение метода объекта и его вызов в разных строках
   let hi = userName.hi;
   hi(); 
+
+  // =================
+
+let genericAnimal  = Object.create(null);
+
+genericAnimal.item = 'Zwierzę';
+genericAnimal.name = 'Nazwa zwierzęta';
+genericAnimal.gender = 'Kobiece';
+genericAnimal.descr = function () {
+    return `Прототип: ${genericAnimal.item}, у которого название - ${this.name}. У него пол: ${this.gender}`;
+};
+
+let wolf = Object.create(genericAnimal);
+wolf.name = 'Wolf';
+wolf.gender = 'Męski';
+wolf.eats = 'rabbit';
+
+
+let andeanWolf = Object.create(wolf);
+andeanWolf.country = 'Andes';
+andeanWolf.name = 'Andean Wolf';
+
+
+let canisFerox = Object.create(wolf);
+canisFerox.country = 'North America';
+
+console.log(andeanWolf.descr());
+
+// =====================
+
+let animal = {
+    whereLived: 'North America',
+    eats: true,
+    predator: "хищник",
+    isPredator() {
+        return `Это животное ${this.predator}.`;
+    }
+
+};
+
+let rabbit = {
+    jumps: true,
+    food: 'carrot',
+    predator: "не хищник"
+
+};
+
+rabbit.__proto__ = animal;
+
+let eagle = {
+    canFly: true,
+    food: 'mouse',
+    __proto__: animal,
+};
+
+eagle.isPredator = function() {
+    return "Орёл любит есть мышей, поэтому он хищник!";
+};
+
+console.log(eagle.whereLived);
