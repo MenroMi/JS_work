4b676944702144866d202362930d055ef8db7164
Cyryl <folkbread@gmail.com>
2022-05-26 21:48:27 +0200

    end of the project 1

--- projects/Mozilla_works/sandpit_mozilla/checked.js
+++ projects/Mozilla_works/sandpit_mozilla/checked.js
@@ -18,3 +18,66 @@ console.log(user.fullName('Alice Cooper')); // John Smith (*)
 admin.fullName('Alice Cooper'); // (**)
 console.log(admin.name); // Alice
 console.log(admin.surname); // Cooper
+
+// ===============================
+
+
+let a = {
+  name: 'John',
+  isAdmin: true,
+  age: 30,
+  oneFunc(keyCheck) {
+    if (keyCheck === false) {
+        a.isAdmin = true;
+        console.log('wartość zmiennej: ' + a.isAdmin);
+    } else if (keyCheck === true) {
+        a.isAdmin = false;
+        console.log('wartość zmiennej: ' + a.isAdmin);
+    }
+  }
+};
+
+
+
+
+a.oneFunc(a.isAdmin);
+console.log(a.isAdmin);
+a.oneFunc(a.isAdmin);
+console.log(a.isAdmin);
+a.oneFunc(a.isAdmin);
+console.log(a.isAdmin);
+
+
+// =====================================
+let a = {
+  firstname: 'John',
+  genres: []
+};
+
+
+let writeYourGenres = () => {
+  
+  // 4
+  for ( let k = 1; k < 2; k++) {
+
+      let answerUser = 'Фантастика,Боевик,драма';
+      if ( answerUser == null || answerUser == '' || +answerUser) {
+          console.log('Введены некорректные данные');
+          k--;
+      } else {
+          
+          a.genres = answerUser.split(',');
+          // a.genres = answerUser;
+      }
+
+  }
+
+  a.genres.forEach((key, i) => {
+      console.log(`Любимый жанр #${i+1} - это ${key}`);
+    });
+  // personalMovieDB.genres.pop();
+};
+
+writeYourGenres();
+console.log(a.genres);
+
