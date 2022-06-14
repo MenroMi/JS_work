096adeb39a2347c7c074eb2026070267184fb264
Cyryl <folkbread@gmail.com>
2022-06-14 21:56:04 +0200

    recursion

--- projects/Mozilla_works/sandpit_mozilla/Recursion/playWithRecursion.js
+++ projects/Mozilla_works/sandpit_mozilla/Recursion/playWithRecursion.js
@@ -31,23 +31,59 @@ function getTotalProgressByIteration(data) {
     let total = 0;
     let students = 0;
 
+
     for (let course of Object.values(data)) {
+
         if (Array.isArray(course)) {
             students += course.length;
             
             for ( let i = 0; i < course.length; i++) {
-                total += course[i].progress;
+                total += course[i].progress; // wybieramy drugą właściwość włożonego obiektu
             }
-            
             // course.forEach((numb) => {
             // total += numb.progress;
             // });
-        }    
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                for (let k = 0; k < subCourse.length; k++) {
+                    total += subCourse[k].progress;
+                }
+            }
+        }
     }
 
-    return total;
+    return total / students;
     // return total / students;
 
 }
 
-console.log(getTotalProgressByIteration(students));
+
+// console.log(getTotalProgressByIteration(students));
+
+function getTotalProgressByRecursion(data) {
+    if (Array.isArray(data)) {
+        let total = 0;
+        
+        for ( let i = 0; i < data.length; i++) {
+            total += data[i].progress;
+        }
+
+        return [total, data.length];
+    } else {
+        let total = [0, 0];
+
+        for ( let subData of Object.values(data)) {
+            const subDataArray = getTotalProgressByRecursion(subData);
+            total[0] += subDataArray[0];
+            total[1] += subDataArray[1];
+        }
+        
+        return total;
+    }
+}
+
+const result = getTotalProgressByRecursion(students);
+
+console.log(result[0]/result[1]);
