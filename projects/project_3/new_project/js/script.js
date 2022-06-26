dd75aed34be8320ff868732cd515f85c7bce3595
Cyryl <folkbread@gmail.com>
2022-06-26 20:18:15 +0200

    new project_3

---
+++ projects/project_3/new_project/js/script.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+
+    function hideTabContent() { // function, which hide all useless tabs
+
+        tabsContent.forEach(tab => { // we iterate, cause we want work
+        // with all element
+            tab.classList.add('hide'); // later we hide useless tabs 
+            tab.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(tab => { // class-active will be turn off with all
+            // off tabs
+
+            tab.classList.remove("tabheader__item_active");
+        });
+
+    }
+
+    function showTabContent(numb = 0) { // we show only active tab
+        // function have default parametr - this is parametr when 
+        // we don't announce function with argument before
+
+        tabsContent[numb].classList.add('show', 'fade');
+        tabsContent[numb].classList.remove('hide');
+        tabs[numb].classList.add("tabheader__item_active"); // we add
+        // our tab activity class
+
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+        // console.log(e);
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((tab, i) => {
+                if (target == tab) {
+                    console.log(tab);
+                    console.log(target);
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+});
