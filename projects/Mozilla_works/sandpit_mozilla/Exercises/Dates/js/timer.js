e0a14d153a5ea9fb64e7e893505629092477dfe3
Cyryl <folkbread@gmail.com>
2022-07-05 23:37:29 +0200

    add day of wee

--- projects/Mozilla_works/sandpit_mozilla/Exercises/Dates/js/timer.js
+++ projects/Mozilla_works/sandpit_mozilla/Exercises/Dates/js/timer.js
@@ -1,7 +1,8 @@
 const futureDate = "2023-07-05",
-      addMonth = new Date().getMonth();
+      addMonth = new Date().getMonth(),
+      addDay = new Date().getDate();
 
-function diffTimeCalculate(endtime, month) {
+function diffTimeCalculate(endtime, month, dayweek) {
     const t = Date.parse(endtime) - Date.now(),
           days = Math.floor((t / (1000 * 60 * 60 * 24))),
           hours = Math.floor((t / (1000 * 60 * 60) % 24)),
@@ -11,6 +12,7 @@ function diffTimeCalculate(endtime, month) {
     return {
         "total": t,
         "months": month,
+        "dayOfWeek": dayweek,
         "days": days,
         "hours": hours,
         "minutes": minutes,
@@ -23,10 +25,11 @@ function addZero(timer) {
 }
 
 
-function setdiffTime(endtime, month) {
+function setdiffTime(endtime, month, day) {
  
     const timer = document.querySelector('.until'),
           months = timer.querySelector('.until__event--month'),
+          dayOfWeek = timer.querySelector('.until__event--day'),
           days = timer.querySelector('.until__numeric--days'),
           hours = timer.querySelector('.until__numeric--hours'),
           minutes = timer.querySelector('.until__numeric--minutes'),
@@ -37,11 +40,12 @@ function setdiffTime(endtime, month) {
 
     function update() {
 
-        const t = diffTimeCalculate(endtime, month);
-        const arrMonths = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.' ];
+        const t = diffTimeCalculate(endtime, month, day),
+              arrMonths = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.' ];
         
         
         months.innerHTML = arrMonths[t.months];
+        dayOfWeek.innerHTML = addZero(t.dayOfWeek);
         days.innerHTML = addZero(t.days);
         hours.innerHTML = addZero(t.hours);
         minutes.innerHTML = addZero(t.minutes);
@@ -54,4 +58,4 @@ function setdiffTime(endtime, month) {
 
 }
 
-setdiffTime(futureDate, addMonth);
+setdiffTime(futureDate, addMonth, addDay);
