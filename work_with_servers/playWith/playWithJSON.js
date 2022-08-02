67e05226586466ac01e0987f577da708d18108ff
Cyryl <folkbread@gmail.com>
2022-08-02 22:00:21 +0200

    work with AJAX

--- work_with_servers/playWith/playWithJSON.js
+++ work_with_servers/playWith/playWithJSON.js
@@ -13,3 +13,26 @@ anotherWorker.parents.dad = 'Justin';
 
 console.log(worker);
 console.log(anotherWorker);
+
+// ========================
+
+let room = {
+    number: 23
+  };
+  
+let meetup = {
+    title: "Совещание",
+    occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+    place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+// console.log(meetup === meetup.self);
+// console.log(meetup.self);
+
+for ( let value in meetup) {
+    console.log(meetup[value]);
+}
