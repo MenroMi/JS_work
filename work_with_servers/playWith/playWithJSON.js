4437258243108e8ff948e6a1ba1ff405e84600ef
Cyryl <folkbread@gmail.com>
2022-08-06 00:27:35 +0200

    exercises

--- work_with_servers/playWith/playWithJSON.js
+++ work_with_servers/playWith/playWithJSON.js
@@ -49,3 +49,75 @@ function breakCamelCase(str) {
 }
 
 breakCamelCase("camelCase");
+
+// =====================
+
+function noReapetLetter(s) {
+    let res = [];
+    const l = s.toLowerCase();
+    if (s.length == 0 || +s) {
+        return "None";
+    } else {
+        for ( let i = 0; i < s.length; i++) {
+            let count = 0;
+            for ( let k = 0; k < s.length; k++) {
+                if (l[i] == l[k]) {
+                    count++;
+                }
+            }
+    
+            if ( count == 1) {
+                res.push(s[i]);
+            }
+        }
+    }
+    
+    return res.length > 0 ? res[0] : 'None';
+}
+
+console.log(noReapetLetter('sTreSS'));
+
+function firstNonRepeatingLetter(s) {
+    for(var i in s) {
+      if(s.match(new RegExp(s[i],"gi")).length === 1) {
+        return s[i];
+      }
+    }
+    return '';
+}
+
+console.log(firstNonRepeatingLetter('stress'));
+
+//==============================
+
+function func(s) {
+    for ( let i in s) {
+        console.log(s[i]);
+    }
+}
+
+func('smith');
+
+
+
+
+// let res = [];
+// if (word.length == 0 || +word) {
+//     console.log('None');
+// } else {
+//     for ( let i = 0; i < word.length; i++) {
+//         let count = 0;
+//         for ( let k = 0; k < word.length; k++) {
+//             if (word[i] == word[k]) {
+//                 count++;
+//             }
+//         }
+
+//         if ( count == 1) {
+//             res.push(word[i]);
+//         } else {
+//             return "None";
+//         }
+//     }
+// }
+// console.log(res[0]);
