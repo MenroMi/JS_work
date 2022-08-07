3a69bd035f646a909613ba9aa10022c7ecd98d83
Cyryl <folkbread@gmail.com>
2022-08-07 21:04:16 +0200

    training promises

--- work_with_servers/promise.js
+++ work_with_servers/promise.js
@@ -24,6 +24,7 @@ req.then((prod) => { // argumentem jest rezultat początkowego promisu
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             prod.status = 'order';
+            console.log('Добавляем новые свойства');
             resolve(prod);
         }, 2000);
     });
@@ -49,3 +50,123 @@ req.then((prod) => { // argumentem jest rezultat początkowego promisu
     }, 1500);
 });
 
+// ===========================
+
+
+const car = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+
+        const audi = {
+            model: "rs8",
+            year: 1998,
+
+        };
+
+        resolve(audi);
+
+    }, 2000);
+
+}).then(infoAboutCar => {
+
+    setTimeout(() => {
+        console.log(infoAboutCar);
+    }, 2000);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+                
+        infoAboutCar.quantity = 4;
+        infoAboutCar.status = 'in stock';
+        resolve(infoAboutCar);
+        
+        }, 2000);
+    });
+
+}).then(res => {
+    setTimeout(() => {
+        console.log(res);
+    }, 2000);
+});
+
+// ==============================
+
+const promise = new Promise((resolve, reject) => {
+
+        resolve(`First...`);
+
+
+}).then(res => {
+    console.log(res);
+    return res;
+}).then(res => {
+    setTimeout(() => {
+    console.log(res);
+    }, 2000);
+    return res += `Second...`;
+
+}).then(res => {
+    setTimeout(() => {
+    console.log(res);
+    }, 3000);
+    return res += `Third...`;
+});
+
+// =====================
+
+const p1 = new Promise((resolve, reject)=> {
+        console.log('Начало...');
+
+        setTimeout(() => {
+
+            resolve("Загрузка...");
+
+        }, 2000);
+
+
+}).then((item) => {
+    console.log(item);
+    setTimeout(() => {
+        console.log('Уже почти...');
+    }, 2000);
+
+    return item;
+
+}).then(() => {
+
+    setTimeout(() => {
+        console.log('Уже-уже...');
+    }, 3000);
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('Пол шишички...');
+
+            reject();
+        }, 4000);
+
+    });
+}).catch(() => {
+    setTimeout(() => {
+        console.error('Ошибка =)');
+
+    }, 2000);
+});
+
+// ===============
+
+const test = time => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+};
+
+test(2000).then(() => {
+    console.log('done - 2000 ms.');
+});
+test(3000).then(() => {
+    console.log('done - 3000 ms.');
+});
