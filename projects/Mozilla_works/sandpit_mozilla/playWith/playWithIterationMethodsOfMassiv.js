126693888a1b18e290fce16eec1071d18b1e33e4
Cyryl <folkbread@gmail.com>
2022-08-11 23:34:09 +0200

    exercises with methods iteration massiv

--- projects/Mozilla_works/sandpit_mozilla/playWith/playWithIterationMethodsOfMassiv.js
+++ projects/Mozilla_works/sandpit_mozilla/playWith/playWithIterationMethodsOfMassiv.js
@@ -37,15 +37,83 @@
 // console.log(res);
 
 
-const obj = {
-    ivan: 'persone',
-    ann: 'persone',
-    dog: 'animal',
-    cat: 'animal'
-};
+// const obj = {
+//     ivan: 'persone',
+//     ann: 'persone',
+//     dog: 'animal',
+//     cat: 'animal'
+// };
 
 // const func = (object, value) => {
 //     return Object.keys(object).find(key => object[key] === value); // find keys from massiv by value
 // };
 
 // console.log(func(obj, 'persone'));
+
+// ===========================================
+
+/*
+1) У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
+
+P.S. Это довольно типичная задача в программировании. Вспомните, на самых разных сайтах можно так фильтровать любые товары/фильмы/сериалы...
+*/
+
+function findFilmsMoreThanEight(arr) {
+    // return arr.filter(film => film.rating >= 8); // 1
+    // return arr.map(film => film.name).reduce((sum, current) => `${sum}, ${current}`); 2
+}
+
+function checkFilms(arr) {
+
+    console.log(arr.every(film => "id" in film));
+
+
+}
+
+function setFilmdIds(arr, checkFilms) {
+
+    let tranformedArray = [];
+
+    for ( let i = 0; i < arr.length; i++) {
+        arr[i].id = i;
+        tranformedArray.push(arr[i]);
+    }
+
+    checkFilms(tranformedArray);
+
+}
+
+
+setFilmdIds([
+    {name: "Joker", rating: 9}, 
+    {name: "Batmam", rating: 10}, 
+    {name: "Superman", rating: 5}, 
+    {name: "Ironman", rating: 3}
+], checkFilms);
+
+
+/*
+2) Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
+
+Пример:
+
+showListOfFilms(films) => "Titanic, Die hard 5, Matrix, Some bad film"
+*/
+
+/*
+3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
+
+Пример:
+
+setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   { name: 'Some bad film', rating: 4, id: 3 } ]
+*/
+
+
+/*
+4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray); :)
+*/
+
+
+function mapMethod(arr) {
+    
+}
--- projects/Mozilla_works/sandpit_mozilla/playWith/site/js/playWithCallBackAndPromise.js
+++ projects/Mozilla_works/sandpit_mozilla/playWith/site/js/playWithCallBackAndPromise.js
@@ -1,53 +1,14 @@
-const obj = {
-    apple: 2,
-    grape: 4,
-    bananas: 1
-};
+function getUsers(names) {
 
-const formData = new FormData(obj);
+    names.forEach(name => {
 
-// let item = prompt('What are you want?', 'friut or vegetable...'),
-//     howMuch = prompt('How much?', 'say please...');
+        fetch(`https://api.github.com/users/${name}`)
+        .then(response => response.json())
+        .then(data => console.log(data));
+        
+    }); 
 
-
-// checkAndCalc(obj, item, addNewItem);
-
-// function findKey(object, value) {
-//     return Object.keys(object).find(key => object[key] === value);
-// }
-
-// function addNewItem(obj, item) {
-
-//     let key = findKey(obj, obj[item]),
-//         value = obj[item];
-
-//     return {
-//         [key]: value,
-//     };
-// }
-
-// function checkAndCalc(object, key, addNewItem) {
-//     setTimeout(() => {
-//         alert('Hmm, wait a minute');
-//         setTimeout(() => {
-
-//             addNewItem(object, key);
-
-//         }, 2000);
-
-//     }, 1000);
-
-// }
-
-const jsonObj = {};
-formData.forEach((value, key) => {
-    jsonObj[key] = value;
-});
+}
 
 
-
-fetch("site.php", {
-    method: "POST",
-    headers: {"Content-type": "application/json"},
-    body: JSON.stringify(jsonObj),
-}).then(response => response.text()).then(item => console.log(item));
+getUsers(['MenroMi', "dnksebastian", "KarolinaBrzezinska"]);
