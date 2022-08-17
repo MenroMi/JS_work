d4849750ba25dfdd272b61f06a472b98a789e04b
Cyryl <folkbread@gmail.com>
2022-08-17 22:51:11 +0200

    axios work

--- work_with_servers/AJAX_work/code_02/js/script.js
+++ work_with_servers/AJAX_work/code_02/js/script.js
@@ -9,17 +9,14 @@ function recieveForm(data) {
        
         e.preventDefault();
 
-        const form = new FormData(data);
+        const formData = new FormData(data);
 
-        const json = JSON.stringify(Object.fromEntries(form.entries()));
+        const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-        fetch("http://localhost:3000/requests", {
-            method: "POST",
-            headers: {"Content-type": "application/json"},
-            body: json
-        })
-        .then(response => response.json())
-        .then(response => console.log(response.name));
+        axios.post(" http://localhost:3000/requests", json)
+        .then(response => console.log(response));
+
+
     });
 
 
