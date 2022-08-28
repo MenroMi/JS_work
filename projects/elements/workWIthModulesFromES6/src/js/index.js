d3983d6bf77a992bbecb32d84781399c6938ca1b
Cyryl <folkbread@gmail.com>
2022-08-28 21:18:40 +0200

    ES6 modules

---
+++ projects/elements/workWIthModulesFromES6/src/js/index.js
@@ -0,0 +1,4 @@
+import {number, second, sayHi as hello} from "./main.js";
+
+console.log(number, second);
+hello();
---
+++ projects/elements/workWIthModulesFromES6/src/js/main.js
@@ -0,0 +1,8 @@
+export let number = 1;
+
+let second = 2; // name of export
+export {second};
+
+export function sayHi() {
+    console.log('Hello!');
+}
