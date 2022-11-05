1a3a06fc714060bd4d5935b9430a00923784a946
Cyryl <folkbread@gmail.com>
2022-11-05 18:09:00 +0100

    work lifecircle of ui5

--- work_with_js/sap-ui5-walkthrough/webapp/controller/App.controller.js
+++ work_with_js/sap-ui5-walkthrough/webapp/controller/App.controller.js
@@ -1,12 +1,28 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ],
 
- (Controller, MessageToast) => {
+ (Controller, MessageToast, JSONModel) => {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+        onInit: function() {
+
+            let oData = {
+                user: {
+                    name: "John",
+                    age: 38
+                }
+            }
+
+            let oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+
+        },
+
         onShowHello: function() {
             MessageToast.show("Hello World!");    
         }
